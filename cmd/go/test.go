d31111da3b3a94e3b22fe191e86b8ae0333bd994
xxnmxx <xxnmxx@gmail.com>
2019-12-29 09:44:49 +0900

    yay

--- cmd/go/test.go
+++ cmd/go/test.go
@@ -18,9 +18,13 @@ func main() {
 	e0 := acc.Equity{200, 1000}
 	e1 := acc.Equity{200, 1000}
 
+	i0 := acc.Industory{92, [5]int{395, 369, 332, 319, 308}, 5.5, 45.0, 232.0}
+	i1 := acc.Industory{94, [5]int{444, 374, 289, 279, 270}, 5.2, 36.0, 175.0}
 	corp := acc.Prof{
-		s,[2]acc.Div{d0,d1},[3]acc.Rev{r0,r1,r2},[2]acc.Equity{e0,e1},
+		s, [2]acc.Div{d0, d1}, [3]acc.Rev{r0, r1, r2}, [2]acc.Equity{e0, e1},
+		[2]acc.Industory{i0, i1}, 1.0,
 	}
 	fmt.Println(corp)
-	fmt.Println(corp.ICalv())
+	fmt.Println(corp.ICalc())
+	fmt.Println(corp.Eval())
 }
--- stkVal.go
+++ stkVal.go
@@ -1,10 +1,12 @@
 package acc
 
 type Prof struct {
-	Stock  Stock
-	Div    [2]Div
-	Rev    [3]Rev
-	Equity [2]Equity
+	Stock     Stock
+	Div       [2]Div
+	Rev       [3]Rev
+	Equity    [2]Equity
+	Industory [2]Industory
+	L         float64
 }
 
 type Stock struct {
@@ -30,7 +32,40 @@ type Equity struct {
 	TaxRE      int
 }
 
-func (p *Prof) ICalv() []float64 {
+type Industory struct {
+	Code int
+	A    [5]int
+	B    float64
+	C    float64
+	D    float64
+}
+
+func (p *Prof) Eval() []float64 {
+	idx := p.ICalc()
+	adj := float64(p.Equity[0].TaxCapital * 1000 / (p.Stock.Issued - p.Stock.Treasury))
+	vs := []float64{}
+
+	for i := range p.Industory {
+		a := float64(p.Industory[i].selector())
+		b := RoundDown(idx[0]/p.Industory[i].B, 1.0)
+		c := RoundDown(idx[1]/p.Industory[i].C, 1.0)
+		d := RoundDown(idx[2]/p.Industory[i].D, 1.0)
+		prepv := RoundDown(a*(b+c+d)/3.0, 1.0)
+		prepv2 := 0.0
+		if p.L == 0.5 {
+			prepv2 = RoundDown(prepv*0.5, 1.0)
+		} else if p.L == 1 {
+			prepv2 = RoundDown(prepv*0.7, 1.0)
+		} else {
+			prepv2 = RoundDown(prepv*0.6, 1.0)
+		}
+		pv := prepv2 * adj / 50.0
+		vs = append(vs, pv)
+	}
+	return vs
+}
+
+func (p *Prof) ICalc() []float64 {
 	divs := []int{}
 	revs := []int{}
 	eqs := []int{}
@@ -39,12 +74,12 @@ func (p *Prof) ICalv() []float64 {
 		d := div.vCalc()
 		divs = append(divs, d)
 	}
-	
+
 	for _, rev := range p.Rev {
 		r := rev.vCalc()
 		revs = append(revs, r)
 	}
-	vrevs := revSelect(revs)
+	vrevs := revSelector(revs)
 
 	for _, eq := range p.Equity {
 		e := eq.vCalc()
@@ -52,15 +87,14 @@ func (p *Prof) ICalv() []float64 {
 	}
 
 	shares := float64(p.Equity[0].TaxCapital * 1000 / 50)
+	preidiv := RoundDown(float64((divs[0]+divs[1])/2), 0.0)
 
-	idiv := RoundDown(float64(divs[0]) / shares, 2.0)
-	irev := RoundDown(float64(vrevs[0]) / shares, 2.0)
-	ieq := RoundDown(float64(eqs[0])/ shares, 2.0)
+	idiv := RoundDown(preidiv*1000/shares, 1.0)
+	irev := RoundDown(float64(vrevs[0]*1000)/shares, 0.0)
+	ieq := RoundDown(float64(eqs[0]*1000)/shares, 0.0)
 
-	is := []float64{idiv,irev,ieq}
-
+	is := []float64{idiv, irev, ieq}
 	return is
-
 }
 
 func (r *Rev) vCalc() int {
@@ -78,22 +112,32 @@ func (e *Equity) vCalc() int {
 	return v
 }
 
-func revSelect(r []int )[]int{
+func (ind *Industory) selector() int {
+	a := ind.A[0]
+	for _, v := range ind.A {
+		if a > v {
+			a = v
+		}
+	}
+	return a
+}
+
+func revSelector(r []int) []int {
 	r00 := r[0]
-	r05 := int(RoundDown(float64((r[0] + r[1]) / 2), 2.0))
+	r05 := int(RoundDown(float64((r[0]+r[1])/2), 2.0))
 	r10 := r[1]
-	r15 := int(RoundDown(float64((r[1] + r[2]) / 2), 2.0))
+	r15 := int(RoundDown(float64((r[1]+r[2])/2), 2.0))
 
 	vRevs := []int{}
 	if r00 < r05 {
 		vRevs = append(vRevs, r00)
-	}else{
+	} else {
 		vRevs = append(vRevs, r05)
 	}
 	// For One-element Company test.Get max value.
 	if r10 < r15 {
 		vRevs = append(vRevs, r15)
-	}else{
+	} else {
 		vRevs = append(vRevs, r10)
 	}
 	return vRevs
