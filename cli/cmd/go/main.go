23ceb570eeb582b566376250e218550e20623382
xxnmxx <xxnmxx@gmail.com>
2020-01-01 08:26:10 +0900

    ok

---
+++ cli/cmd/go/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/xxnmxx/acc/cli"
+)
+
+func main() {
+	cli.Input()
+}
---
+++ cli/input.go
@@ -0,0 +1,23 @@
+package cli
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func Input() {
+	fmt.Print(">> ")
+	r := bufio.NewScanner(os.Stdin)
+	for r.Scan() {
+		fmt.Print(">> ")
+		cmd := r.Text()
+		switch cmd {
+		case "exit":
+			break
+		default:
+			fmt.Println(cmd)
+			fmt.Print(">> ")
+		}
+	}
+}
